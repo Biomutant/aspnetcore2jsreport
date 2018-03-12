f4e3fb0edf47f67ab8de0c38a2e899c6ae9c0881
Luis Cano <luecano@gmail.com>
2018-03-11 19:25:36 -0500

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using jsreport.AspNetCore;
+using jsreport.Types;
+using Microsoft.AspNetCore.Mvc;
+using netreport.Model;
+using netreport.Models;
+
+namespace netreport.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IJsReportMVCService JsReportMVCService { get; }
+
+        public HomeController(IJsReportMVCService jsReportMVCService)
+        {
+            JsReportMVCService = jsReportMVCService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+         [MiddlewareFilter(typeof(JsReportPipeline))]
+        public IActionResult Invoice()
+        {
+            HttpContext.JsReportFeature().Recipe(Recipe.PhantomPdf);
+
+            return View(InvoiceModel.Example());
+        }
+
+        [MiddlewareFilter(typeof(JsReportPipeline))]
+        public IActionResult InvoiceDownload()
+        {
+            HttpContext.JsReportFeature().Recipe(Recipe.PhantomPdf)
+                .OnAfterRender((r) => HttpContext.Response.Headers["Content-Disposition"] = "attachment; filename=\"myReport.pdf\"");
+
+            return View("Invoice", InvoiceModel.Example());
+}
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace netreport.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
